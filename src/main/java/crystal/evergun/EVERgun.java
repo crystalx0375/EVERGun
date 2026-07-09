4b79d090b4e0708fa1f514e3891a97c5f730af83
crystalx0375 <crystalx0375@gmail.com>
2026-07-09 20:42:58 +0300

    init

--- src/main/java/crystal/evergun/EVERgun.java
+++ src/main/java/crystal/evergun/EVERgun.java
@@ -1,7 +1,14 @@
 package crystal.evergun;
 
+import crystal.evergun.util.Settings;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
@@ -14,7 +21,8 @@ public class EVERgun implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
-	}
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register((itemGroup) -> itemGroup.add(Settings.GUN));
+    }
 
 	public static Identifier id(String path) {
 		return Identifier.of(MOD_ID, path);
